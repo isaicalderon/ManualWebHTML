1564c4a820a5a4e4a600efb4c2498a9b7d252728
isai.acalderon <isai.acalderon@gmail.com>
2019-03-19 19:03:25 -0700

    v1.0.0

---
+++ code.js
@@ -0,0 +1,7 @@
+//function darColor(string) {
+	String texto = document.getElementById('code-java');
+	//console.log(texto);
+	for (var i = 0; i < texto.length; i++) {
+		console.log(texto.charAt(i));
+	}
+//}
