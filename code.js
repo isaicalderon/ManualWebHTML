475b24b81b2e5ade71f18c0d18068f328d50cc89
isaicalderon <isailacalderon@gmail.com>
2019-03-20 18:30:08 -0700

    v1.0.1

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
 //function darColor(string) {
-	String texto = document.getElementById('code-java');
+	//var texto = document.getElementById('code-java');
 	//console.log(texto);
-	for (var i = 0; i < texto.length; i++) {
-		console.log(texto.charAt(i));
-	}
+
 //}
